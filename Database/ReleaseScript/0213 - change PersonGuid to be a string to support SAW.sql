35aec96bdfefe7255eb4614660369eb61eb88acd
raynardlee <raynardlee@yahoo.com>
2018-12-21 12:23:12 -0800

    adding liz's saw account guid

--- Database/ReleaseScript/0213 - change PersonGuid to be a string to support SAW.sql
+++ Database/ReleaseScript/0213 - change PersonGuid to be a string to support SAW.sql
@@ -15,4 +15,6 @@ update dbo.Person
 set PersonUniqueIdentifier = 'DP3TP7WZ2MM7W-1DZ8DP4Q-1DL2VV0ZF1-D1FV3ZT5VM'
 where PersonUniqueIdentifier = 'CD3DAB18-4242-4FE9-AB10-874CA43AAEE2'
 
-
+update dbo.Person 
+set PersonUniqueIdentifier = 'DP4VW0PV4LL0W-1TW0LT5VV8-D1LW4VZ0FD-FF8FP8MQ1'
+where PersonUniqueIdentifier = '61E2A1D0-6A3F-499C-B72C-2160196006F0'
