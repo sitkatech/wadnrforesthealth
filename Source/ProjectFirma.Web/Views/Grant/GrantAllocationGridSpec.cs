8bca118c07d02418567433cb216d3c36ee93b91c
mike jolliffe <mike.jolliffe@sitkatech.com>
2019-01-18 13:49:13 -0800

    adding ProgramIndex to GrantAllocation gridspec

--- Source/ProjectFirma.Web/Views/Grant/GrantAllocationGridSpec.cs
+++ Source/ProjectFirma.Web/Views/Grant/GrantAllocationGridSpec.cs
@@ -46,6 +46,7 @@ namespace ProjectFirma.Web.Views.Grant
             Add("End Date", x => x.EndDate.ToShortDateString(), 90, DhtmlxGridColumnFilterType.SelectFilterStrict);
             Add("Allocation Amount", x => x.AllocationAmount.ToStringCurrency(), 90, DhtmlxGridColumnFilterType.SelectFilterStrict);
             Add("Project Codes", x => x.ProjectCodesString, 90, DhtmlxGridColumnFilterType.Text);
+            Add("Program Index", x => x.ProgramIndex != null ? x.ProgramIndex.ProgramIndexAbbrev : string.Empty, 90, DhtmlxGridColumnFilterType.SelectFilterStrict);
         }
     }
 }
