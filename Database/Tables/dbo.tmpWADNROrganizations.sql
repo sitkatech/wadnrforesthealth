aa8b84fb4fd44e7badf75b1eca322bf7bff5c718
mike jolliffe <mike.jolliffe@sitkatech.com>
2019-01-28 16:43:06 -0800

    [wa_dnr_forest_health_tracker/#1469] Import organizations -- Importing orgs into staging table.

---
+++ Database/Tables/dbo.tmpWADNROrganizations.sql
@@ -0,0 +1,9 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[tmpWADNROrganizations](
+	[Name] [nvarchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[ShortName] [nvarchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[OrganizationType] [nvarchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
