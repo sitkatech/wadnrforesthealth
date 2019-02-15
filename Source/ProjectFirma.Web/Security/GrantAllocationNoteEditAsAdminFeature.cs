fb9c78df01f42cdd75274b25b015014c1680ec94
Tom Kamin <tomkamin@gmail.com>
2019-02-14 16:55:00 -0800

    [wa_dnr_forest_health_tracker/#1485] Provide a way to edit grant notes moved new security feature classes to their own files

---
+++ Source/ProjectFirma.Web/Security/GrantAllocationNoteEditAsAdminFeature.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using ProjectFirma.Web.Models;
+
+namespace ProjectFirma.Web.Security
+{
+    [SecurityFeatureDescription("Edit Grant Allocation Note")]
+    public class GrantAllocationNoteEditAsAdminFeature : FirmaFeatureWithContext, IFirmaBaseFeatureWithContext<GrantAllocationNote>
+    {
+        private readonly FirmaFeatureWithContextImpl<GrantAllocationNote> _firmaFeatureWithContextImpl;
+        private readonly GrantAllocationEditAsAdminFeature grantAllocationEditAsAdminFeature;
+
+        public GrantAllocationNoteEditAsAdminFeature()
+            : base(new List<Role> { Role.SitkaAdmin, Role.Admin })
+        {
+            _firmaFeatureWithContextImpl = new FirmaFeatureWithContextImpl<GrantAllocationNote>(this);
+            grantAllocationEditAsAdminFeature = new GrantAllocationEditAsAdminFeature();
+            ActionFilter = _firmaFeatureWithContextImpl;
+        }
+
+        public PermissionCheckResult HasPermission(Person person, GrantAllocationNote contextModelObject)
+        {
+            return grantAllocationEditAsAdminFeature.HasPermission(person, contextModelObject.GrantAllocation);
+        }
+
+        public void DemandPermission(Person person, GrantAllocationNote contextModelObject)
+        {
+            _firmaFeatureWithContextImpl.DemandPermission(person, contextModelObject);
+        }
+    }
+}
--- Source/ProjectFirma.Web/Security/GrantEditAsAdminFeature.cs
+++ Source/ProjectFirma.Web/Security/GrantEditAsAdminFeature.cs
@@ -3,33 +3,6 @@ using ProjectFirma.Web.Models;
 
 namespace ProjectFirma.Web.Security
 {
-
-    [SecurityFeatureDescription("Edit Grant Note")]
-    public class GrantNoteEditAsAdminFeature : FirmaFeatureWithContext, IFirmaBaseFeatureWithContext<GrantNote>
-    {
-        private readonly FirmaFeatureWithContextImpl<GrantNote> _firmaFeatureWithContextImpl;
-        private readonly GrantEditAsAdminFeature grantEditAsAdminFeature;
-
-        public GrantNoteEditAsAdminFeature()
-            : base(new List<Role> { Role.SitkaAdmin, Role.Admin })
-        {
-            _firmaFeatureWithContextImpl = new FirmaFeatureWithContextImpl<GrantNote>(this);
-            grantEditAsAdminFeature = new GrantEditAsAdminFeature();
-            ActionFilter = _firmaFeatureWithContextImpl;
-        }
-
-        public PermissionCheckResult HasPermission(Person person, GrantNote contextModelObject)
-        {
-            return grantEditAsAdminFeature.HasPermission(person, contextModelObject.Grant);
-        }
-
-        public void DemandPermission(Person person, GrantNote contextModelObject)
-        {
-            _firmaFeatureWithContextImpl.DemandPermission(person, contextModelObject);
-        }
-    }
-
-
     [SecurityFeatureDescription("Edit Grant")]
     public class GrantEditAsAdminFeature : FirmaFeatureWithContext, IFirmaBaseFeatureWithContext<Grant>
     {
