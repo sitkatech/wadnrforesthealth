68ce2f8966d1ed9ee76ba4ed5cf2dea1d9e3d10e
tomkamin <tom.kamin@sitkatech.com>
2019-02-25 14:49:03 -0800

    [wa_dnr_forest_health_tracker/#1495] Capture Statewide Vendor Number on Organization adding sproc to import vendor json data

--- Database/ReleaseScript/0311 - adding vendor table and FKs to it on Person and Organization.sql
+++ Database/ReleaseScript/0311 - adding vendor table and FKs to it on Person and Organization.sql
@@ -1,24 +1,23 @@
 create table dbo.Vendor (
-	VendorID int not null constraint PK_Vendor_VendorID primary key,
+	VendorID int not null constraint PK_Vendor_VendorID primary key identity(1,1),
 	VendorName varchar(100) not null,
 	StatewideVendorNumber varchar(20) not null,
-	VendorSuffix varchar(10) not null,
-	CONSTRAINT AK_Vendor_StatewideVendorNumber_VendorSuffix UNIQUE (StatewideVendorNumber, VendorSuffix)
+	StatewideVendorNumberSuffix varchar(10) not null,
+	CONSTRAINT AK_Vendor_StatewideVendorNumber_StatewideVendorNumberSuffix UNIQUE (StatewideVendorNumber, StatewideVendorNumberSuffix)
 );
 
-
-insert dbo.Vendor (VendorID, VendorName, StatewideVendorNumber, VendorSuffix) 
+insert dbo.Vendor (VendorName, StatewideVendorNumber, StatewideVendorNumberSuffix) 
 values 
-(1, 'Atg Daily Deposit', 'SWV0006858', '06'),
-(2, 'Atg Document Services', 'SWV0006858', '07'),
-(3, 'Atg Gse Transfer', 'SWV0006858', '09'),
-(4, 'Brent Murphy Inc', 'SWV0005422', '00'),
-(5, 'Brent Murphy Inc', 'SWV0005422', '01'),
-(6, 'Clark County Clerk', 'SWV0003051', '22'),
-(7, 'Dairy Fresh Farms Inc', 'SWV0005671', '00'),
-(8, 'Dept of Commerce', 'SWV0000116', '00'),
-(9, 'Dept of Commerce', 'SWV0000116', '01'),
-(10, 'DOC Petty Cash 001', 'SWV0003872', '59')
+('Atg Daily Deposit', 'SWV0006858', '06'),
+('Atg Document Services', 'SWV0006858', '07'),
+('Atg Gse Transfer', 'SWV0006858', '09'),
+('Brent Murphy Inc', 'SWV0005422', '00'),
+('Brent Murphy Inc', 'SWV0005422', '01'),
+('Clark County Clerk', 'SWV0003051', '22'),
+('Dairy Fresh Farms Inc', 'SWV0005671', '00'),
+('Dept of Commerce', 'SWV0000116', '00'),
+('Dept of Commerce', 'SWV0000116', '01'),
+( 'DOC Petty Cash 001', 'SWV0003872', '59')
 
 
 
