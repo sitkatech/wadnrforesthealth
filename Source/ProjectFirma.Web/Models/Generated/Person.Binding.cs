3cccf312e4135ef2fd6457be2cb69b90145122e2
StewLGSitka <stewart@sitkatech.com>
2019-02-26 13:32:15 -0800

    [wa_dnr_forest_health_tracker/#1493] Import sample Agreement Contacts OrganizationID can't be made non-nullable on Person, at least not without some work I don't want to do quite yet.

--- Source/ProjectFirma.Web/Models/Generated/Person.Binding.cs
+++ Source/ProjectFirma.Web/Models/Generated/Person.Binding.cs
@@ -64,7 +64,7 @@ namespace ProjectFirma.Web.Models
         /// <summary>
         /// Constructor for building a new object with MaximalConstructor required fields in preparation for insert into database
         /// </summary>
-        public Person(int personID, string personUniqueIdentifier, string firstName, string lastName, string email, string phone, string passwordPdfK2SaltHash, int roleID, DateTime createDate, DateTime? updateDate, DateTime? lastActivityDate, bool isActive, int organizationID, bool receiveSupportEmails, Guid? webServiceAccessToken, string loginName, string middleName, string statewideVendorNumber, string notes, string personAddress, int? addedByPersonID) : this()
+        public Person(int personID, string personUniqueIdentifier, string firstName, string lastName, string email, string phone, string passwordPdfK2SaltHash, int roleID, DateTime createDate, DateTime? updateDate, DateTime? lastActivityDate, bool isActive, int? organizationID, bool receiveSupportEmails, Guid? webServiceAccessToken, string loginName, string middleName, string statewideVendorNumber, string notes, string personAddress, int? addedByPersonID) : this()
         {
             this.PersonID = personID;
             this.PersonUniqueIdentifier = personUniqueIdentifier;
@@ -92,7 +92,7 @@ namespace ProjectFirma.Web.Models
         /// <summary>
         /// Constructor for building a new object with MinimalConstructor required fields in preparation for insert into database
         /// </summary>
-        public Person(string firstName, string lastName, int roleID, DateTime createDate, bool isActive, int organizationID, bool receiveSupportEmails) : this()
+        public Person(string firstName, string lastName, int roleID, DateTime createDate, bool isActive, bool receiveSupportEmails) : this()
         {
             // Mark this as a new object by setting primary key with special value
             this.PersonID = ModelObjectHelpers.MakeNextUnsavedPrimaryKeyValue();
@@ -102,14 +102,13 @@ namespace ProjectFirma.Web.Models
             this.RoleID = roleID;
             this.CreateDate = createDate;
             this.IsActive = isActive;
-            this.OrganizationID = organizationID;
             this.ReceiveSupportEmails = receiveSupportEmails;
         }
 
         /// <summary>
         /// Constructor for building a new object with MinimalConstructor required fields, using objects whenever possible
         /// </summary>
-        public Person(string firstName, string lastName, Role role, DateTime createDate, bool isActive, Organization organization, bool receiveSupportEmails) : this()
+        public Person(string firstName, string lastName, Role role, DateTime createDate, bool isActive, bool receiveSupportEmails) : this()
         {
             // Mark this as a new object by setting primary key with special value
             this.PersonID = ModelObjectHelpers.MakeNextUnsavedPrimaryKeyValue();
@@ -118,18 +117,15 @@ namespace ProjectFirma.Web.Models
             this.RoleID = role.RoleID;
             this.CreateDate = createDate;
             this.IsActive = isActive;
-            this.OrganizationID = organization.OrganizationID;
-            this.Organization = organization;
-            organization.People.Add(this);
             this.ReceiveSupportEmails = receiveSupportEmails;
         }
 
         /// <summary>
         /// Creates a "blank" object of this type and populates primitives with defaults
         /// </summary>
-        public static Person CreateNewBlank(Role role, Organization organization)
+        public static Person CreateNewBlank(Role role)
         {
-            return new Person(default(string), default(string), role, default(DateTime), default(bool), organization, default(bool));
+            return new Person(default(string), default(string), role, default(DateTime), default(bool), default(bool));
         }
 
         /// <summary>
@@ -358,7 +354,7 @@ namespace ProjectFirma.Web.Models
         public DateTime? UpdateDate { get; set; }
         public DateTime? LastActivityDate { get; set; }
         public bool IsActive { get; set; }
-        public int OrganizationID { get; set; }
+        public int? OrganizationID { get; set; }
         public bool ReceiveSupportEmails { get; set; }
         public Guid? WebServiceAccessToken { get; set; }
         public string LoginName { get; set; }
