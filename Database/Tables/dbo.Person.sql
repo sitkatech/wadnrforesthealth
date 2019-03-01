d21ae4ddcc74d575f0246ef81a6d83a51f7e4cee
StewLGSitka <stewart@sitkatech.com>
2019-02-28 21:19:06 -0800

    Wadnr: Fixing FK gen-all

--- Database/Tables/dbo.Person.sql
+++ Database/Tables/dbo.Person.sql
@@ -40,10 +40,10 @@ CREATE UNIQUE NONCLUSTERED INDEX [IX_Person_Email_UniqueWhenNotNull] ON [dbo].[P
 WHERE ([Email] IS NOT NULL)
 WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[Person]  WITH CHECK ADD  CONSTRAINT [FK_Person_Authenticator_AuthenticatorID] FOREIGN KEY([AllowedAuthenticatorID])
+ALTER TABLE [dbo].[Person]  WITH CHECK ADD  CONSTRAINT [FK_Person_Authenticator_AllowedAuthenticatorID_AuthenticatorID] FOREIGN KEY([AllowedAuthenticatorID])
 REFERENCES [dbo].[Authenticator] ([AuthenticatorID])
 GO
-ALTER TABLE [dbo].[Person] CHECK CONSTRAINT [FK_Person_Authenticator_AuthenticatorID]
+ALTER TABLE [dbo].[Person] CHECK CONSTRAINT [FK_Person_Authenticator_AllowedAuthenticatorID_AuthenticatorID]
 GO
 ALTER TABLE [dbo].[Person]  WITH CHECK ADD  CONSTRAINT [FK_Person_Organization_OrganizationID] FOREIGN KEY([OrganizationID])
 REFERENCES [dbo].[Organization] ([OrganizationID])
