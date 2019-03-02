22885056e501cacd1379d66238d37db3e0b969d4
StewLGSitka <stewart@sitkatech.com>
2019-03-01 19:13:48 -0800

    Wadnr: Security tweakage.

--- Source/ProjectFirma.Web/Models/Person.DatabaseContextExtensions.cs
+++ Source/ProjectFirma.Web/Models/Person.DatabaseContextExtensions.cs
@@ -57,7 +57,7 @@ namespace ProjectFirma.Web.Models
             var person = people.ToList().SingleOrDefault(p => IsMatchingPersonEnvironmentCredentials(p, desiredPersonUniqueIdentifier, desiredDeploymentEnvironment, desiredAuthenticator));
 
             string successString = person != null ? "Found" : "Did NOT find";
-            SitkaHttpApplication.Logger.Debug($"GetPersonByPersonUniqueIdentifier: {successString} desiredPersonUniqueIdentifier: \"{desiredPersonUniqueIdentifier}\" - desiredDeploymentEnvironment: {desiredDeploymentEnvironment.DeploymentEnvironmentName} - desiredAuthenticator : {desiredAuthenticator.AuthenticatorFullName}");
+            SitkaHttpApplication.Logger.Debug($"GetPersonByPersonUniqueIdentifier: {successString} desiredPersonUniqueIdentifier: \"{desiredPersonUniqueIdentifier}\" - desiredDeploymentEnvironment: {desiredDeploymentEnvironment.DeploymentEnvironmentName} - desiredAuthenticator : {desiredAuthenticator.AuthenticatorName}");
             return person;
         }
 
