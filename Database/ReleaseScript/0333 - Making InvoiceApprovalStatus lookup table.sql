f2082668f9815e87de998bcecfc620cd2167a114
StewLGSitka <stewart@sitkatech.com>
2019-03-18 17:38:16 -0700

    WADNR - 03/18/2019 release - Clearing out release scripts

--- Database/ReleaseScript/0333 - Making InvoiceApprovalStatus lookup table.sql
+++
@@ -1,7 +0,0 @@
-delete from dbo.InvoiceApprovalStatus
-
-insert dbo.InvoiceApprovalStatus (InvoiceApprovalStatusID, InvoiceApprovalStatusName) 
-values 
-(1, 'Not Set'),
-(2, 'Approved'),
-(3, 'Denied')
