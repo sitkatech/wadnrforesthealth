4b6a22e680671b072ef4bfd9ac4f3fb23cf7ae0e
tomkamin <tom.kamin@sitkatech.com>
2019-04-04 14:11:34 -0700

    add some null checks to region extension methods

--- Source/ProjectFirma.Web/Models/Region.cs
+++ Source/ProjectFirma.Web/Models/Region.cs
@@ -45,14 +45,14 @@ namespace ProjectFirma.Web.Models
         public Feature MakeFeatureWithRelevantProperties()
         {
             var feature = DbGeometryToGeoJsonHelper.FromDbGeometry(RegionLocation);
-            feature.Properties.Add("Region", GetDisplayNameAsUrl().ToString());
+            feature.Properties.Add("Region", this.GetRegionDisplayNameAsUrl().ToString());
             return feature;
         }
 
-        public HtmlString GetDisplayNameAsUrl()
-        {
-            return UrlTemplate.MakeHrefString(GetDetailUrl(), DisplayName);
-        }
+        //public HtmlString GetDisplayNameAsUrl()
+        //{
+        //    return UrlTemplate.MakeHrefString(GetDetailUrl(), DisplayName);
+        //}
 
         public string GetDetailUrl()
         {
