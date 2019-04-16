6a0436534388158a76b6a77bbc6536ddddc9f156
StewLGSitka <stewart@sitkatech.com>
2019-04-16 15:18:57 -0700

    * [wa_dnr_forest_health_tracker/#1578] Integrate Datamart cost type data - Ok, fixing up historic records. Vendor still needs to be output.

--- Database/ReleaseScript/0370 - Altering dbo.ProgramIndex to handle biennium natively, ditching IsHistoric.sql
+++ Database/ReleaseScript/0370 - Altering dbo.ProgramIndex to handle biennium natively, ditching IsHistoric.sql
@@ -1,15 +1,6 @@
 --begin tran
 
 
-
---insert into ProgramIndex (ProgramIndexAbbrev, ProgramIndexTitle, IsHistoric)
---values 
---('216', '216-TRAINING 2014 CPG', 1),
---('21B', '21B-TRAINING 2015 CPG', 1),
---('25N', '25N-2011 CPG WIND AND WHITE SALMON RIVER', 1)
-
-
-
 alter table dbo.ProgramIndex
 add Biennium int null
 GO
@@ -27,30 +18,12 @@ update dbo.ProgramIndex
 set Biennium = 2019
 where Biennium is null and IsHistoric = 0
 
-
-
 /*
 ProgramIndexID	ProgramIndexCode	ProgramIndexTitle	Biennium	Activity	Program	Subprogram	Subactivity
 603	00216	216-TRAINING 2014 CPG	2017	01	020	01	06
 608	0021B	21B-TRAINING 2015 CPG	2017	01	020	01	07
 653	0025N	25N-2011 CPG WIND AND WHITE SALMON RIVER	2017	03	020	05	06
 */
-
--- These are wired to all-null ProgramIndexes. They make no sense; unhooking them and killing them.
---update GrantAllocation
---set ProgramIndexID = null
---where ProgramIndexID in (4,5,10)
-
--- Now we can kill the Historic Program Indexes. These are NOT found in the feed from WA DNR! It would be best to fix them, but we'll see if that proves possible.
---delete from dbo.ProgramIndex
---where IsHistoric = 1 and Biennium is null
-
-
-
-
---select * from GrantAllocation where ProgramIndexID in (4,5,10)
-
---select * from dbo.ProgramIndex where Biennium is null
 
 alter table dbo.ProgramIndex
 drop column IsHistoric
