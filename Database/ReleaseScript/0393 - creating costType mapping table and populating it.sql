e8c3d56bc2d2f7b76a7ebdc28fa8bc9a08efd292
tomkamin <tom.kamin@sitkatech.com>
2019-05-13 16:37:55 -0700

    [wa_dnr_forest_health_tracker/#1578] Integrate Datamart cost type data created mapping table for cost type

---
+++ Database/ReleaseScript/0393 - creating costType mapping table and populating it.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE [dbo].[CostTypeDatamartMapping](
+	[CostTypeDatamartMappingID] [int] NOT NULL,
+	CostTypeID int not null constraint FK_CostTypeDatamartMapping_CostType_CostTypeID references dbo.CostType(CostTypeID),
+	DatamartObjectCode [varchar](10) NOT NULL,
+	DatamartObjectName varchar(100) NOT NULL,
+ CONSTRAINT [PK_CostTypeDatamartMapping_CostTypeDatamartMappingID] PRIMARY KEY CLUSTERED 
+(
+	[CostTypeDatamartMappingID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+Insert into dbo.CostTypeDatamartMapping (CostTypeDatamartMappingID, CostTypeID, DatamartObjectCode, DatamartObjectName)
+values
+(1, 3, 'A', 'SALARIES AND WAGES'),
+(2, 3, 'B', 'EMPLOYEE BENEFITS'),
+(3, 4, 'G', 'TRAVEL'),
+(4, 5, 'N', 'GRANTS, BENEFITS & CLIENT SERVICES')
