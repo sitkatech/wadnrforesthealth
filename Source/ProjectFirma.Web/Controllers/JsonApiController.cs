099b6e36a84814d4c8192fc139d60eb651c0115f
StewLGSitka <stewart@sitkatech.com>
2019-05-15 18:10:16 -0700

    https://wadnrforesthealth.localhost.projectfirma.com/JsonApiManagement/JsonApiLandingPage now works.

---
+++ Source/ProjectFirma.Web/Controllers/JsonApiController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using ProjectFirma.Web.Models;
+using ProjectFirma.Web.Security;
+using ProjectFirma.Web.Views.JsonApiManagement;
+
+namespace ProjectFirma.Web.Controllers
+{
+    public class JsonApiManagementController : FirmaBaseController
+    {
+        [ViewJsonApiLandingPageFeature]
+        public ViewResult JsonApiLandingPage()
+        {
+            var firmaPage = FirmaPage.GetFirmaPageByPageType(FirmaPageType.TagList);
+            var viewData = new Views.JsonApiManagement.JsonApiLandingPageViewData(CurrentPerson, firmaPage);
+            return RazorView<JsonApiLandingPage, JsonApiLandingPageViewData>(viewData);
+        }
+    }
+}
