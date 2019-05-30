57ea6d8e1f42c72874f1372d39c13f3c212dd3d0
StewLGSitka <stewart@sitkatech.com>
2019-05-29 17:31:28 -0700

    [wa_dnr_forest_health_tracker/#1578] Integrate Datamart cost type data It think we are going to ditch the merge for now, and just delete everything each time, but I wanted to keep track of this work in the fossil record, just in case.

---
+++ Source/LtInfo.Common/BienniumTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace LtInfo.Common
+{
+    [TestFixture]
+    public class BienniumTest
+    {
+        // public static int 
+
+        [Test]
+        public void GetBienniumForFiscalYearTest()
+        {
+            // Prior years
+            Assert.That(DateUtilities.GetBienniumForGivenFiscalYear(2015) == 2015);
+            Assert.That(DateUtilities.GetBienniumForGivenFiscalYear(2016) == 2015);
+            Assert.That(DateUtilities.GetBienniumForGivenFiscalYear(2017) == 2017);
+            Assert.That(DateUtilities.GetBienniumForGivenFiscalYear(2018) == 2017);
+
+            // "Current" year (happens to be reference year code uses)
+            Assert.That(DateUtilities.GetBienniumForGivenFiscalYear(2019) == 2019);
+
+            // Future years
+            Assert.That(DateUtilities.GetBienniumForGivenFiscalYear(2020) == 2019);
+            Assert.That(DateUtilities.GetBienniumForGivenFiscalYear(2021) == 2021);
+            Assert.That(DateUtilities.GetBienniumForGivenFiscalYear(2022) == 2021);
+            Assert.That(DateUtilities.GetBienniumForGivenFiscalYear(2023) == 2023);
+            Assert.That(DateUtilities.GetBienniumForGivenFiscalYear(2024) == 2023);
+
+        }
+    }
+}
