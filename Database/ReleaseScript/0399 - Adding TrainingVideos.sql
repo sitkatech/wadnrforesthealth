7e95ef7ab14a767d6c90bd0d4d752f66fde3bb3a
Michael Ferrante <michael@sitkatech.com>
2019-05-31 16:15:54 -0700

    [wa_dnr_forest_health_tracker] Clearing out database scripts post release * clearing out release scripts to reflect prod release today * removing no longer needed script "adding a firma page for fullagreementspagetype.sql"

--- Database/ReleaseScript/0399 - Adding TrainingVideos.sql
+++
@@ -1,6 +0,0 @@
-insert into dbo.TrainingVideo (VideoName, VideoDescription, VideoUploadDate, VideoURL) values 
-( 'Washington DNR Forest Health Tracker Overview', '', '5/31/2019', 'https://www.youtube.com/embed/cJcATtcmw_o'),
-( 'User and Account Management', '', '5/31/2019', 'https://www.youtube.com/embed/lL0BrRuYS2k'),
-( 'Adding and Updating a Project', '', '5/31/2019', 'https://www.youtube.com/embed/ewpvA_Hj_XE'),
-( 'Adding an Invoice', '', '5/31/2019', 'https://www.youtube.com/embed/hceRDCzyaQQ'),
-( 'Adding a Grant', '', '5/31/2019', 'https://www.youtube.com/embed/20A9TGtD2Pg')
