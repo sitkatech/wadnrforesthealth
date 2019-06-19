f424b87d138cdfd7ea43d9bcbadd82fde5adfe0f
tomkamin <tom.kamin@sitkatech.com>
2019-06-19 14:58:08 -0700

    [wa_dnr_forest_health_tracker/#1639] Capture Grant Allocation Award Cost Types creating security features for grant allocation award contractor invoices

---
+++ Source/ProjectFirma.Web/Security/GrantAllocationAwardContractorInvoiceCreateFeature.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ProjectFirma.Web.Models;
+
+namespace ProjectFirma.Web.Security
+{
+    [SecurityFeatureDescription("Create a Grant Allocation Award Contractor Invoice", FieldDefinitionEnum.GrantAllocationAwardContractorInvoice)]
+    public class GrantAllocationAwardContractorInvoiceCreateFeature : FirmaFeature
+    {
+        public GrantAllocationAwardContractorInvoiceCreateFeature()
+            : base(new List<Role> { Role.SitkaAdmin, Role.Admin, Role.ProjectSteward })
+        {
+        }
+    }
+}
---
+++ Source/ProjectFirma.Web/Security/GrantAllocationAwardContractorInvoiceDeleteFeature.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using ProjectFirma.Web.Models;
+
+namespace ProjectFirma.Web.Security
+{
+    [SecurityFeatureDescription("Delete Grant Allocation Award Contractor Invoice")]
+    public class GrantAllocationAwardContractorInvoiceDeleteFeature : FirmaFeatureWithContext, IFirmaBaseFeatureWithContext<GrantAllocationAwardContractorInvoice>
+    {
+        private readonly FirmaFeatureWithContextImpl<GrantAllocationAwardContractorInvoice> _firmaFeatureWithContextImpl;
+
+        public GrantAllocationAwardContractorInvoiceDeleteFeature()
+            : base(new List<Role> { Role.SitkaAdmin, Role.Admin })
+        {
+            _firmaFeatureWithContextImpl = new FirmaFeatureWithContextImpl<GrantAllocationAwardContractorInvoice>(this);
+            ActionFilter = _firmaFeatureWithContextImpl;
+        }
+
+        public void DemandPermission(Person person, GrantAllocationAwardContractorInvoice contextModelObject)
+        {
+            _firmaFeatureWithContextImpl.DemandPermission(person, contextModelObject);
+        }
+
+        public PermissionCheckResult HasPermission(Person person, GrantAllocationAwardContractorInvoice contextModelObject)
+        {
+            var hasPermissionByPerson = HasPermissionByPerson(person);
+            if (!hasPermissionByPerson)
+            {
+                return PermissionCheckResult.MakeFailurePermissionCheckResult($"You don't have permission to delete {FieldDefinition.GrantAllocationAwardContractorInvoice} with ID: {contextModelObject.GrantAllocationAwardContractorInvoiceDescription}");
+            }
+            return PermissionCheckResult.MakeSuccessPermissionCheckResult();
+        }
+    }
+}
