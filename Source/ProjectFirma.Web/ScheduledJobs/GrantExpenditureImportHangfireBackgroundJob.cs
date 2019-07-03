a93e401a158c54ac3e1bb4986845f57ebe864b6f
StewLGSitka <stewart@sitkatech.com>
2019-07-02 17:24:31 -0700

    Wadnr: Tammy tells me she has repaired this data issue via email, and indeed the job now seems to run successfully without it. Removing this code. Probably won't run to move this to prod, but it could move at any time we wanted.

--- Source/ProjectFirma.Web/ScheduledJobs/GrantExpenditureImportHangfireBackgroundJob.cs
+++ Source/ProjectFirma.Web/ScheduledJobs/GrantExpenditureImportHangfireBackgroundJob.cs
@@ -118,16 +118,6 @@ namespace ProjectFirma.Web.ScheduledJobs
             Logger.Info($"Attempting to retrieve Expenditures for Biennium Fiscal Year {bienniumFiscalYear} from URL {fullUrl}...");
             string grantExpenditureJson = DownloadSocrataUrlToString(fullUrl, SocrataDataMartRawJsonImportTableType.GrantExpenditure);
 
-            // HACK -- We are finding bad data like this in 2007 :   "DocDate": "0905-11-08T00:00:00"
-            //         In this ONE CASE, we will surgically replace the "0905" year with 2005, which it what it apparently should be.
-            //         We target it narrowly because we'd want to be attentive if the problem gets any worse / changes / moves to other years, etc.
-            //         (We have asked Tammy Osborn @ WA DNR to fix this, but we'd like to move on an silence relevant errors in the meantime. 
-            //         This code can be removed once Tammy fixes the data upstream.)
-            if (bienniumFiscalYear == 2007)
-            {
-                grantExpenditureJson = grantExpenditureJson.Replace("\"DocDate\":\"0905-11-08T00:00:00\"", "\"DocDate\":\"2005-11-08T00:00:00\"");
-            }
-
             Logger.Info($"GrantExpenditure BienniumFiscalYear {bienniumFiscalYear} JSON length: {grantExpenditureJson.Length}");
             // Push that string into a raw JSON string in the raw staging table
             int socrataDataMartRawJsonImportID = ShoveRawJsonStringIntoTable(SocrataDataMartRawJsonImportTableType.GrantExpenditure, lastFinanceApiLoadDate, bienniumFiscalYear, grantExpenditureJson);
