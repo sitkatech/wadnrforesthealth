8e9ac6f6b9b2e590e87edf932bdc37be98e1ebcb
tomkamin <tom.kamin@sitkatech.com>
2019-11-11 16:12:30 -0800

    [wa_dnr_forest_health_tracker/#1667] Bug: error on "Program Index Import" background job sleeping job until 12/3/2019

--- Source/ProjectFirma.Web/ScheduledJobs/ProgramIndexImportHangfireBackgroundJob.cs
+++ Source/ProjectFirma.Web/ScheduledJobs/ProgramIndexImportHangfireBackgroundJob.cs
@@ -47,9 +47,9 @@ namespace ProjectFirma.Web.ScheduledJobs
 
         public void DownloadSocrataProgramIndexTable()
         {
-            if (DateTime.Compare(DateTime.Now, new DateTime(2019, 11, 11)) < 0)
+            if (DateTime.Compare(DateTime.Now, new DateTime(2019, 12, 3)) < 0)
             {
-                Logger.Info($"We have some issues with Program Index data coming from the feed. Skipping job '{JobName}' until 11/11/2019");
+                Logger.Info($"We have some issues with Program Index data coming from the feed. Skipping job '{JobName}' until 12/03/2019");
                 return;
             }
             Logger.Info($"Starting '{JobName}' DownloadSocrataProgramIndexTable");
