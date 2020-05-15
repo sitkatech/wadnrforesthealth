fb8a621d6e2e98f166decbbd1585f215f36cd841
stewartgordonsitka <stewart.gordon@sitkatech.com>
2020-05-14 17:37:15 -0700

    WADNR-1672 Update DNR logo on prod and qa * Updating release script to grab the logo that was hotfixed out to prod and QA

--- Database/ReleaseScript/422 - adding new wadnr logo.sql
+++ Database/ReleaseScript/422 - adding new wadnr logo.sql
@@ -1,31 +1,11 @@
-
-DECLARE @fileBinary varbinary(max)
-
-IF @@SERVERNAME = 'WALLOWA' or @@SERVERNAME = 'KETTLE'
-    SET @fileBinary =  (SELECT bulkcolumn
-        FROM OPENROWSET( BULK N'C:\Sitka\WADNRForestHealth\wwwroot\Content\img\wadnr-logo-cropped.png', SINGLE_BLOB ) AS y );
-ELSE
-    SET @fileBinary =  (SELECT bulkcolumn
-        FROM OPENROWSET( BULK N'C:\git\sitkatech\wadnrforesthealth\Source\ProjectFirma.Web\Content\img\wadnr-logo-cropped.png', SINGLE_BLOB ) AS y );
-    
-insert into dbo.FileResource (FileResourceMimeTypeID, OriginalBaseFilename, OriginalFileExtension, FileResourceGUID, FileResourceData, CreatePersonID, CreateDate)
-values
-(
-    5,
-    'Washington_State_Department_of_Natural_Resources_logo',
-    '.png',
-    NEWID(),
-    @fileBinary,
-    5233,
-    CURRENT_TIMESTAMP
-);
-
 update dbo.SystemAttribute
-set BannerLogoFileResourceID = SCOPE_IDENTITY()
+set BannerLogoFileResourceID = (select FileResourceID from dbo.FileResource where OriginalBaseFilename = 'Washington_State_Department_of_Natural_Resources_logo')
 where SystemAttributeID = 10
 
 /*
 
 select * from SystemAttribute
+
+select * from dbo.FileResource where OriginalBaseFilename = 'Washington_State_Department_of_Natural_Resources_logo'
 
 */
