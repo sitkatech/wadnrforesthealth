716828fee237d6d335e6c86b8d6e4857d1e62909
StewLGSitka <stewart@sitkatech.com>
2020-09-14 09:12:12 -0700

    WADNR: Fixing IAuditable crash (POST /Project/DeleteProject/13517 NullReferenceException)

---
+++ Source/ProjectFirma.Web/Models/Treatment.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjectFirma.Web.Models
+{
+    public partial class Treatment : IAuditableEntity
+    {
+        public string AuditDescriptionString =>  $"TreatmentID:{this.TreatmentID} -  TreatmentAreaID:{this.TreatmentArea.TreatmentAreaID}  - TreatmentType:{this.TreatmentType.TreatmentTypeDisplayName} - {this.TreatmentStartDate} - {this.TreatmentEndDate}";
+    }
+}
