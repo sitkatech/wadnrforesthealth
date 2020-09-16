136484059d83814fb529b78b12aa31745964a292
StewLGSitka <stewart@sitkatech.com>
2020-09-16 12:05:25 -0700

    WADNR-1770 - Can't delete imported projects * Fixing Treatment Audit properly this time. This is what I get for not actually testing.

--- Source/ProjectFirma.Web/Models/Treatment.cs
+++ Source/ProjectFirma.Web/Models/Treatment.cs
@@ -2,6 +2,6 @@
 {
     public partial class Treatment : IAuditableEntity
     {
-        public string AuditDescriptionString =>  $"TreatmentID:{this.TreatmentID} -  TreatmentAreaID:{this.TreatmentArea.TreatmentAreaID}  - TreatmentType:{this.TreatmentType.TreatmentTypeDisplayName} - {this.TreatmentStartDate} - {this.TreatmentEndDate}";
+        public string AuditDescriptionString =>  $"TreatmentID:{this.TreatmentID} -  TreatmentAreaID:{this.TreatmentAreaID}  - TreatmentTypeID:{this.TreatmentTypeID} - :TreatmentStartDate: {this.TreatmentStartDate} - TreatmentEndDate: {this.TreatmentEndDate}";
     }
 }
