34c171d6dd188343331d77f527cdf300bf7e0f99
tomkamin <tom.kamin@sitkatech.com>
2021-02-18 17:25:01 -0800

    post-release release script removal

--- Database/ReleaseScript/0519 - updating loa defaults.sql
+++
@@ -1,8 +0,0 @@
-
-
-
-update dbo.GisUploadSourceOrganization
-set ProjectTypeDefaultName = 'Integrated forest health project',
-    AdjustProjectTypeBasedOnTreatmentTypes = 1
-where GisUploadSourceOrganizationID = 3
-
--- Database/ReleaseScript/0524 - setting bit to apply completion date to state lands.sql
+++
@@ -1,4 +0,0 @@
-
-update dbo.GisUploadSourceOrganization
-set ApplyCompletedDateToProject = 1
-where GisUploadSourceOrganizationID in (7,5)
