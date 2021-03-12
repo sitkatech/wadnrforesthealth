9a362d46b5f2c1568b75e366a88017c97ff64733
john-vivio <john.vivio@sitkatech.com>
2021-03-12 08:15:36 -0800

    fixing some usfs mapping issues

--- Database/ReleaseScript/0534 - adding crosswalks for timber harvest.sql
+++ Database/ReleaseScript/0534 - adding crosswalks for timber harvest.sql
@@ -1,48 +1,54 @@
 
-insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
-select 11, 469,'Chipping','Chipping'
-
-insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
-select 11, 469,'Mastication/Mowing','Mastication'
-
-
-insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
-select 11, 469,'Grazing','Grazing'
-
-insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
-select 11, 469,'Lop and Scatter',	'Lop and Scatter'
 
 insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
 select 11, 469,'Biomass Removal','Biomass Removal'
 
 insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
-select 11, 469,'Broadcast Burn',	'Broadcast Burn'
-
-insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
 select 11, 469,'Machine Pile',	'Machine Pile'
-
-insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
-select 11, 469,'Machine Pile Burn','Machine Pile Burn'
-
 
 insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
 select 11, 469,'Jackpot Burn'	,'Jackpot Burn'
 
 insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
-select 11, 469,'Thinning',	'Thinning'
+select 11, 469,'Commercial Thinning',	'Thinning'
 
 insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
-select 11, 469,'Planting',	'Planting'
+select 11, 469,'Liberation Cut',	'Thinning'
 
 
-insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
-select 11, 469,'Chemical'	, 'Other' 
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Seed-tree cut (w/res' , 'Other'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Salvage cut (interme' , 'Other'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Single-tree selectio' , 'Other'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Shelterwood cut (EA/' , 'Other'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Group selection cut'  , 'Other'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Sanitation Cut'       , 'Other'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Stand clearcutting (' , 'Other'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Shelterwood preparat' , 'Other'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Seed-tree final remo' , 'Other'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Final overstory remo' , 'Other'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Shelterwood Final Re' , 'Other'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Overstory removal cu' , 'Other'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Seed tree preparator' , 'Other'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Seed-tree seed cut (' , 'Other'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 469, 'Single-tree selectio' , 'Other'
 
-insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
-select 11, 469,'N/A'	, 'Other' 
 
-insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
-select 11, 469,'Fire Use'	, 'Other' 
-
-insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue)
-select 11, 469,'Crushing'	, 'Other' 
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Seed-tree cut (w/res', 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Salvage cut (interme', 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Commercial Thinning' , 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Single-tree selectio', 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Shelterwood cut (EA/', 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Group selection cut' , 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Sanitation Cut'      , 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Stand clearcutting (', 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Shelterwood preparat', 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Seed-tree final remo', 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Final overstory remo', 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Liberation Cut'      , 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Shelterwood Final Re', 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Overstory removal cu', 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Seed tree preparator', 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Seed-tree seed cut (', 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Single-tree selectio', 'Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Biomass Removal', 'Non-Commercial'
+insert into dbo.GisCrossWalkDefault(GisUploadSourceOrganizationID,FieldDefinitionID,GisCrossWalkSourceValue, GisCrossWalkMappedValue) select 11, 468,'Machine Pile', 'Non-Commercial'
