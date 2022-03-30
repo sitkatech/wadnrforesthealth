bb7187e8050628557731d18570a1ce97198ef821
tomkamin <tom.kamin@sitkatech.com>
2022-03-29 17:31:03 -0700

    WADNR-1878 Update permissions model to be more flexible using internal endpoint that hits WAMAS server for geocoding

--- Source/ProjectFirma.Web/Controllers/ResultsController.cs
+++ Source/ProjectFirma.Web/Controllers/ResultsController.cs
@@ -21,8 +21,12 @@ Source code is available upon request via <support@sitkatech.com>.
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.ServiceModel;
 using System.Web.Mvc;
+using System.Xml;
 using ProjectFirma.Web.Security;
 using ProjectFirma.Web.Common;
 using ProjectFirma.Web.Models;
@@ -125,6 +129,84 @@ namespace ProjectFirma.Web.Controllers
                                                   projectMapLocationJsons);
             return RazorView<ProjectMap, ProjectMapViewData>(viewData);
         }
+
+        [HttpPost]
+        public JsonNetJArrayResult GeocodeAddress(string address, string zip)
+        {
+            //https://wamas.watech.wa.gov/geocoder/service.asmx/FindAddress?address=&Consumer=FHT&zip=97210&city=&zip4=null
+
+            //http://geography.wa.gov/GeospatialPortal/FindAddress
+            //
+
+            var _url = "https://wamas.watech.wa.gov/geocoder/service.asmx";
+            var _action = "http://geography.wa.gov/GeospatialPortal/FindAddress";
+
+            XmlDocument soapEnvelopeXml = CreateSoapEnvelope(address, zip);
+            HttpWebRequest webRequest = CreateWebRequest(_url, _action);
+            InsertSoapEnvelopeIntoWebRequest(soapEnvelopeXml, webRequest);
+
+            // begin async call to web request.
+            IAsyncResult asyncResult = webRequest.BeginGetResponse(null, null);
+
+            // suspend this thread until call is complete. You might want to
+            // do something usefull here like update your UI.
+            asyncResult.AsyncWaitHandle.WaitOne();
+
+            // get the response from the completed web request.
+            string soapResult;
+            using (WebResponse webResponse = webRequest.EndGetResponse(asyncResult))
+            {
+                using (StreamReader rd = new StreamReader(webResponse.GetResponseStream()))
+                {
+                    soapResult = rd.ReadToEnd();
+                }
+                //Console.Write(soapResult);
+            }
+            //need to parse soapResult XML into json object to pass to frontend
+
+            return new JsonNetJArrayResult(string.Empty);
+        }
+
+
+        private static HttpWebRequest CreateWebRequest(string url, string action)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+            webRequest.Headers.Add("SOAPAction", action);
+            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+            webRequest.Accept = "text/xml";
+            webRequest.Method = "POST";
+            return webRequest;
+        }
+
+        private static XmlDocument CreateSoapEnvelope(string address, string zip)
+        {
+            XmlDocument soapEnvelopeDocument = new XmlDocument();
+            soapEnvelopeDocument.LoadXml(
+                @"<SOAP-ENV:Envelope xmlns:SOAP-ENV=""http://schemas.xmlsoap.org/soap/envelope/"" 
+               xmlns:xsi=""http://www.w3.org/1999/XMLSchema-instance"" 
+               xmlns:xsd=""http://www.w3.org/1999/XMLSchema"">
+        <SOAP-ENV:Body>
+            <FindAddress xmlns=""http://geography.wa.gov/GeospatialPortal/"" 
+                SOAP-ENV:encodingStyle=""http://schemas.xmlsoap.org/soap/encoding/"">
+                <address>" + address + @"</address>
+                <zip>" + zip + @"</zip>
+                <city></city>
+                <zip4></zip4>
+                <Consumer>FHT</Consumer>
+            </FindAddress>
+        </SOAP-ENV:Body>
+    </SOAP-ENV:Envelope>");
+            return soapEnvelopeDocument;
+        }
+
+        private static void InsertSoapEnvelopeIntoWebRequest(XmlDocument soapEnvelopeXml, HttpWebRequest webRequest)
+        {
+            using (Stream stream = webRequest.GetRequestStream())
+            {
+                soapEnvelopeXml.Save(stream);
+            }
+        }
+
 
         private static Dictionary<ProjectLocationFilterTypeSimple, IEnumerable<SelectListItem>> CreateProjectLocationFilterTypesAndValuesDictionary(bool showProposals)
         {
