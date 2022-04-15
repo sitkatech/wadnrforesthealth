2982e6118836f763e7abe249beaca347d81eefe7
tomkamin <tom.kamin@sitkatech.com>
2022-04-14 18:39:54 -0700

    reverting context base on this security feature because the reason it was added, I cannot seem to find its usage. This context was also breaking a unit test, because only in some cases did a Grant Allocation create need the organization(context object that was removed in this commit)

--- Source/ProjectFirma.Web/Security/GrantAllocationCreateFeature.cs
+++ Source/ProjectFirma.Web/Security/GrantAllocationCreateFeature.cs
@@ -4,36 +4,15 @@ using ProjectFirma.Web.Models;
 namespace ProjectFirma.Web.Security
 {
     [SecurityFeatureDescription("Create a Grant Allocation", FieldDefinitionEnum.Application)]
-    public class GrantAllocationCreateFeature : FirmaFeatureWithContext, IFirmaBaseFeatureWithContext<Organization>
+    public class GrantAllocationCreateFeature : FirmaFeature
     {
-        private readonly FirmaFeatureWithContextImpl<Organization> _firmaFeatureWithContextImpl;
         public GrantAllocationCreateFeature()
             : base(new List<Role> { Role.SitkaAdmin, Role.Admin, Role.ProjectSteward })
         {
-            _firmaFeatureWithContextImpl = new FirmaFeatureWithContextImpl<Organization>(this);
-            ActionFilter = _firmaFeatureWithContextImpl;
+
 
         }
 
-        public PermissionCheckResult HasPermission(Person person, Organization contextModelObject)
-        {
-            if (person.HasAnyOfTheseRoles(new List<IRole>() {Role.Admin, Role.SitkaAdmin}))
-            {
-                return PermissionCheckResult.MakeSuccessPermissionCheckResult();
-            }
 
-            // If person is project steward, they can only create grant allocations for their organization
-            if (person.HasRole(Role.ProjectSteward) && person.OrganizationID == contextModelObject.OrganizationID)
-            {
-                return PermissionCheckResult.MakeSuccessPermissionCheckResult();
-            }
-
-            return PermissionCheckResult.MakeFailurePermissionCheckResult($"You cannot create a {FieldDefinition.GrantAllocation.GetFieldDefinitionLabel()}  for this {FieldDefinition.Organization.GetFieldDefinitionLabel()}, '{contextModelObject.DisplayName}'.");
-        }
-
-        public void DemandPermission(Person person, Organization contextModelObject)
-        {
-            _firmaFeatureWithContextImpl.DemandPermission(person, contextModelObject);
-        }
     }
 }
