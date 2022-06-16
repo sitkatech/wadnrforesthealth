a739f6fc0d42a07bfc845e9f42cd1cc990aeaf36
tomkamin <tom.kamin@sitkatech.com>
2022-06-16 13:46:12 -0700

    WADNR-1904 Allow addition and edit of treatment activities via the FHT UI updated test script to get distinct treatment areas for project location import

--- Database/ReleaseScript/0570 - migrating Treatment Areas into the Project Location table.sql
+++ Database/ReleaseScript/0570 - migrating Treatment Areas into the Project Location table.sql
@@ -11,21 +11,37 @@ select * from dbo.ProjectLocationType
 select * from dbo.Treatment
 
 */
-	alter table dbo.ProjectLocation
-	add TreatmentAreaID int null
-	go
 
-	--34832 starts the dupes
-	INSERT INTO dbo.ProjectLocation(ProjectID, ProjectLocationGeometry, ProjectLocationTypeID, ProjectLocationName, ProgramID, ImportedFromGisUpload, TreatmentAreaID)
-	SELECT --44,368
-		t.ProjectID as ProjectID,
-		ta.TreatmentAreaFeature as ProjectLocationGeometry,
+
+	----34832 starts the dupes
+	--INSERT INTO dbo.ProjectLocation(ProjectID, ProjectLocationGeometry, ProjectLocationTypeID, ProjectLocationName, ProgramID, ImportedFromGisUpload, TreatmentAreaID)
+	--SELECT distinct --44,368
+	--	t.ProjectID as ProjectID,
+	--	--ta.TreatmentAreaFeature as ProjectLocationGeometry,
+	--	2 as ProjectLocationTypeID,
+	--	concat('Imported Project Location Name ', t.TreatmentAreaID) as ProjectLocationName,
+	--	t.ProgramID as ProgramID,
+	--	1 as ImportedFromGisUpload,
+	--	ta.TreatmentAreaID
+	--  FROM dbo.TreatmentArea as ta
+	--  inner join dbo.Treatment as t on ta.TreatmentAreaID = t.TreatmentAreaID
+	--  order by ProjectLocationName
+
+alter table dbo.ProjectLocation
+add TreatmentAreaID int null
+go
+
+;with UniqueTreatmentAreas as
+(
+	select distinct t.ProjectID as ProjectID,
 		2 as ProjectLocationTypeID,
-		concat('Imported Project Location Name ', t.TreatmentID) as ProjectLocationName,
+		concat('Imported Project Location Name ', t.TreatmentAreaID) as ProjectLocationName,
 		t.ProgramID as ProgramID,
 		1 as ImportedFromGisUpload,
 		ta.TreatmentAreaID
-	  FROM dbo.TreatmentArea as ta
-	  inner join dbo.Treatment as t on ta.TreatmentAreaID = t.TreatmentAreaID
-	  order by ProjectLocationName
-
+	from dbo.TreatmentArea as ta
+		inner join dbo.Treatment as t on ta.TreatmentAreaID = t.TreatmentAreaID
+)
+select uta.*, ta.TreatmentAreaFeature
+from UniqueTreatmentAreas uta
+	inner join TreatmentArea ta on ta.TreatmentAreaID = uta.TreatmentAreaID
