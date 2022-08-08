3f295ce84242c3ae4f465fdcc164af3fd266b1ab
Rex Ounekeo <rounekeo@esassoc.com>
2022-08-08 14:02:22 -0700

    WADNR-1902 Provide Admin ability to block projects from bulk import - Allow block list entries to persist with just Project Name and/or Project GIS Identifier after project delete

--- Source/ProjectFirma.Web/Controllers/ProjectCreateController.cs
+++ Source/ProjectFirma.Web/Controllers/ProjectCreateController.cs
@@ -1121,6 +1121,12 @@ namespace ProjectFirma.Web.Controllers
             {
                 return ViewDeleteProject(project, viewModel);
             }
+
+            //Unlink ProjectImportBlockLists before delete
+            foreach (var blockListEntry in project.ProjectImportBlockLists)
+                blockListEntry.ProjectID = null;
+            project.ProjectImportBlockLists.Clear();
+
             var message = $"{FieldDefinition.Project.GetFieldDefinitionLabel()} \"{project.DisplayName}\" successfully deleted.";
             project.DeleteFull(HttpRequestStorage.DatabaseEntities);
             SetMessageForDisplay(message);
