f556a14534e9a14880347b2db9e859a7321e4ebe
adrianna.mickel <amickel@esassoc.com>
2022-08-08 22:03:54 -0700

    WADNR-1907- Admin can easily manage Find Your Forester jurisdictions/contacts Added script to disable save button when person has not been selected. Added custom dropdown to include an "unassigned" option in addition to the default "choose one"

--- Source/ProjectFirma.Web/Views/FindYourForester/BulkAssignForestersViewData.cs
+++ Source/ProjectFirma.Web/Views/FindYourForester/BulkAssignForestersViewData.cs
@@ -38,7 +38,7 @@ namespace ProjectFirma.Web.Views.FindYourForester
         public BulkAssignForestersViewData(Person currentPerson, List<Person> allPossibleForesters, List<ForesterWorkUnit> foresterWorkUnitIDList)
         {
             ForesterWorkUnitIDList = foresterWorkUnitIDList;
-            AllPossibleForesters = allPossibleForesters.ToSelectListWithEmptyFirstRow(x => x.PersonID.ToString(CultureInfo.InvariantCulture), y => y.FullNameFirstLast, "None"); 
+            AllPossibleForesters = allPossibleForesters.ToSelectListWithEmptyFirstRow(x => x.PersonID.ToString(CultureInfo.InvariantCulture), y => y.FullNameFirstLast, "Unassigned"); 
         }
 
     }
