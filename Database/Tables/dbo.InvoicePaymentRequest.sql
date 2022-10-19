23f143561f033bcfcd3a14ad45e1daabcc302815
tomkamin <tom.kamin@sitkatech.com>
2022-10-18 19:37:11 -0700

    WADNR-2005 Add Invoices section to Project Detail page removing approved by fields from IPR

--- Database/Tables/dbo.InvoicePaymentRequest.sql
+++ Database/Tables/dbo.InvoicePaymentRequest.sql
@@ -12,19 +12,12 @@ CREATE TABLE [dbo].[InvoicePaymentRequest](
 	[Duns] [varchar](20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[InvoicePaymentRequestDate] [datetime] NOT NULL,
 	[Notes] [varchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[ApprovedByPersonID] [int] NULL,
-	[ApprovedDate] [datetime] NULL,
  CONSTRAINT [PK_InvoicePaymentRequest_InvoicePaymentRequestID] PRIMARY KEY CLUSTERED 
 (
 	[InvoicePaymentRequestID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
-GO
-ALTER TABLE [dbo].[InvoicePaymentRequest]  WITH CHECK ADD  CONSTRAINT [FK_InvoicePaymentRequest_Person_ApprovedByPersonID_PersonID] FOREIGN KEY([ApprovedByPersonID])
-REFERENCES [dbo].[Person] ([PersonID])
-GO
-ALTER TABLE [dbo].[InvoicePaymentRequest] CHECK CONSTRAINT [FK_InvoicePaymentRequest_Person_ApprovedByPersonID_PersonID]
 GO
 ALTER TABLE [dbo].[InvoicePaymentRequest]  WITH CHECK ADD  CONSTRAINT [FK_InvoicePaymentRequest_Person_PreparedByPersonID_PersonID] FOREIGN KEY([PreparedByPersonID])
 REFERENCES [dbo].[Person] ([PersonID])
