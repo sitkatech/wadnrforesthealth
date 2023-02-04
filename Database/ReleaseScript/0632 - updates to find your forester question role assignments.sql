f798c972789aaa76ce700cf8e134336c5a0b1990
tomkamin <tom.kamin@sitkatech.com>
2023-02-03 16:02:30 -0800

    WADNR-1996 Updates to Find Your Forester tool updates to find your forester questions and role assignments

---
+++ Database/ReleaseScript/0632 - updates to find your forester question role assignments.sql
@@ -0,0 +1,10 @@
+
+update dbo.FindYourForesterQuestion
+set ForesterRoleID = 1
+where FindYourForesterQuestionID = 10
+
+
+update dbo.FindYourForesterQuestion
+set ForesterRoleID = 5 
+where FindYourForesterQuestionID = 24
+
