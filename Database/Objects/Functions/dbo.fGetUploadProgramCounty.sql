56f42ed6f952f04c017d85f12d24d1dacde8fb9f
Rex Ounekeo <rounekeo@esassoc.com>
2023-02-17 14:37:21 -0800

    WADNR-2018 Automatically set County based on location when importing projects via AGOL and GDB upload - Cleaned up formatting of function

--- Database/Objects/Functions/dbo.fGetUploadProgramCounty.sql
+++ Database/Objects/Functions/dbo.fGetUploadProgramCounty.sql
@@ -10,39 +10,47 @@ CREATE FUNCTION dbo.fGetUploadProgramCounty
 returns table
 as return(
 
-
     select number = ROW_NUMBER() OVER (ORDER BY x.ProjectID, x.CountyID), x.ProjectID, x.CountyID 
-    
-    from(
-
-    
-     select distinct x.ProjectID, county.CountyID
-
-    from dbo.ProjectLocation pl
-        join (
-        select distinct p.CreateGisUploadAttemptID
-        , p.LastUpdateGisUploadAttemptID
-        , p.ProjectID
-        , pl.ProjectLocationID 
-        , p.ProjectGisIdentifier
-        , pp.ProgramID
+    from
+    (
+        select distinct x.ProjectID, county.CountyID
         from dbo.ProjectLocation pl
-            join dbo.Project p on pl.ProjectID = p.ProjectID
-            join dbo.ProjectProgram pp on pp.ProjectID = p.ProjectID
-        where pp.ProgramID = @programID
+        join 
+        (
+            select distinct p.CreateGisUploadAttemptID
+            , p.LastUpdateGisUploadAttemptID
+            , p.ProjectID
+            , pl.ProjectLocationID 
+            , p.ProjectGisIdentifier
+            , pp.ProgramID
+            from dbo.ProjectLocation pl
+                join dbo.Project p on pl.ProjectID = p.ProjectID
+                join dbo.ProjectProgram pp on pp.ProjectID = p.ProjectID
+            where pp.ProgramID = @programID
         ) x on x.ProjectLocationID = pl.ProjectLocationID
         join dbo.County county on county.CountyFeature.STIntersects(pl.ProjectLocationGeometry) = 1
-         where  x.ProjectGisIdentifier in (select distinct gfma.GisFeatureMetadataAttributeValue from dbo.GisFeature gf 
-                        join dbo.GisFeatureMetadataAttribute gfma on gfma.GisFeatureID = gf.GisFeatureID 
-                        where gfma.GisMetadataAttributeID = @piGisMetadataAttributeID and gf.GisUploadAttemptID = @piGisUploadAttemptID)
-               and x.ProgramID = @programID 
-
-
-
-        ) x 
+        where x.ProjectGisIdentifier in 
+        (
+            select distinct gfma.GisFeatureMetadataAttributeValue 
+            from dbo.GisFeature gf 
+                join dbo.GisFeatureMetadataAttribute gfma on gfma.GisFeatureID = gf.GisFeatureID 
+            where gfma.GisMetadataAttributeID = @piGisMetadataAttributeID and gf.GisUploadAttemptID = @piGisUploadAttemptID
+        )
+        and x.ProgramID = @programID     
+    ) x 
 
 )
 go
 
---select * from dbo.fGetUploadProgramCounty(169, 263, 3)
+
+/*
+
+    --This does not grab data unless there is an upload attempt in progress
+    declare @GisUploadAttemptID int
+    set @GisUploadAttemptID = (select max(GisUploadAttemptID) from dbo.GisUploadAttempt)
+
+    select * from dbo.fGetUploadProgramCounty(@GisUploadAttemptID, 263, 3) 
+
+*/
+
 
