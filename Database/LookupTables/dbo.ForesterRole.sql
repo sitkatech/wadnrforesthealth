a7cf7bb2a637261d95d29112fbab1319a8957d11
tomkamin <tom.kamin@sitkatech.com>
2023-03-24 18:57:47 -0700

    WADNR-2028 Hide some roles on map and right panel of Find Your Forester tool attempting to get roles to hide/show in sidebar

--- Database/LookupTables/dbo.ForesterRole.sql
+++ Database/LookupTables/dbo.ForesterRole.sql
@@ -5,19 +5,19 @@ delete from dbo.ForesterRole
 Insert into dbo.ForesterRole (ForesterRoleID, ForesterRoleDisplayName, ForesterRoleName, SortOrder)
 values
 (1, 'Service Forester', 'ServiceForester', 10),
-(2, 'Service Forestry Specialist', 'ServiceForestrySpecialist', 130),
-(3, 'Forest Practices Forester', 'ForestPracticesForester', 90),
+(2, 'Service Forestry Specialist', 'ServiceForestrySpecialist', 160),
+(3, 'Forest Practices Forester', 'ForestPracticesForester', 80),
 (4, 'Stewardship Fish & Wildlife Biologist', 'StewardshipFishAndWildlifeBiologist', 20),
-(5, 'Urban Forestry Technician', 'UrbanForestryTechnician', 80),
+(5, 'Urban Forestry Technician', 'UrbanForestryTechnician', 120),
 (6, 'Community Resilience Coordinator', 'CommunityResilienceCoordinator', 70),
 (7, 'Regulation Assistance Forester', 'RegulationAssistanceForester', 30),
-(8, 'Family Forest Fish Passage Program', 'FamilyForestFishPassageProgram', 40),
-(9, 'Forestry Riparian Easement Program', 'ForestryRiparianEasementProgram', 50),
-(10, 'Rivers and Habitat Open Space Program Manager', 'RiversAndHabitatOpenSpaceProgramManager', 60),
-(11, 'Service Forestry Program Manager', 'ServiceForestryProgramManager', 100),
-(12, 'UCF Statewide Specialist', 'UcfStatewideSpecialist', 120),
-(13, 'Small Forest Landowner Office Program Manager', 'SmallForestLandownerOfficeProgramManager', 110),
-(14, 'Forest Regulation Fish & Wildlife Biologist', 'ForestRegulationFishAndWildlifeBiologist', 140)
+(8, 'Family Forest Fish Passage Program', 'FamilyForestFishPassageProgram', 90),
+(9, 'Forestry Riparian Easement Program', 'ForestryRiparianEasementProgram', 100),
+(10, 'Rivers and Habitat Open Space Program Manager', 'RiversAndHabitatOpenSpaceProgramManager', 110),
+(11, 'Service Forestry Program Manager', 'ServiceForestryProgramManager', 130),
+(12, 'UCF Statewide Specialist', 'UcfStatewideSpecialist', 150),
+(13, 'Small Forest Landowner Office Program Manager', 'SmallForestLandownerOfficeProgramManager', 140),
+(14, 'Forest Regulation Fish & Wildlife Biologist', 'ForestRegulationFishAndWildlifeBiologist', 60)
 
 
 SET IDENTITY_INSERT dbo.ForesterRole OFF;
