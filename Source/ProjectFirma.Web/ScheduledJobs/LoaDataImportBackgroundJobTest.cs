f17d89732bb83bc34f61a7cd3f5d1f330cfcec3f
Michael Ferrante <michael@sitkatech.com>
2023-04-27 08:54:04 -0700

    WADNR-2038 - Added unit test for LoaDataImportBackgroundJob to be sure it at least runs. Does take 7 minutes, we could consider removing or disabling it if becomes too intensive.

---
+++ Source/ProjectFirma.Web/ScheduledJobs/LoaDataImportBackgroundJobTest.cs
@@ -0,0 +1,16 @@
+﻿using Hangfire;
+using NUnit.Framework;
+
+namespace ProjectFirma.Web.ScheduledJobs
+{
+    [TestFixture]
+    public class LoaDataImportBackgroundJobTest
+    {
+        [Test, Description("Simple test to at least get some coverage of LOA. However test can take over 7 minutes. Can be useful for performance profiling.")]
+        public void WholeJobShouldRunWithoutErrors()
+        {
+            var jobCancellationToken = new JobCancellationToken(false);
+            LoaDataImportBackgroundJob.Instance.RunJob(jobCancellationToken);
+        }
+    }
+}
