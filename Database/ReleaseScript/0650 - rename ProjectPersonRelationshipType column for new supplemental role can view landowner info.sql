0798f009364fc795a1561dda032c679ed1230a26
adrianna.mickel <amickel@esassoc.com>
2023-11-20 17:08:36 -0800

    features/WADNR-2068_Add_Supplemental_Role_Can_view_landowner_info *Remove autogenerated release script unneeded code

--- Database/ReleaseScript/0650 - rename ProjectPersonRelationshipType column for new supplemental role can view landowner info.sql
+++ Database/ReleaseScript/0650 - rename ProjectPersonRelationshipType column for new supplemental role can view landowner info.sql
@@ -1,18 +1,3 @@
-BEGIN TRANSACTION
-SET QUOTED_IDENTIFIER ON
-SET ARITHABORT ON
-SET NUMERIC_ROUNDABORT OFF
-SET CONCAT_NULL_YIELDS_NULL ON
-SET ANSI_NULLS ON
-SET ANSI_PADDING ON
-SET ANSI_WARNINGS ON
-COMMIT
-BEGIN TRANSACTION
-GO
-EXECUTE sp_rename N'dbo.ProjectPersonRelationshipType.IsRestrictedToAdminAndProjectSteward', N'Tmp_IsRestrictedToAdminAndProjectStewardAndCanViewLandownerInfo', 'COLUMN' 
-GO
-EXECUTE sp_rename N'dbo.ProjectPersonRelationshipType.Tmp_IsRestrictedToAdminAndProjectStewardAndCanViewLandownerInfo', N'IsRestrictedToAdminAndProjectStewardAndCanViewLandownerInfo', 'COLUMN' 
-GO
-ALTER TABLE dbo.ProjectPersonRelationshipType SET (LOCK_ESCALATION = TABLE)
-GO
-COMMIT
+
+EXECUTE sp_rename N'dbo.ProjectPersonRelationshipType.IsRestrictedToAdminAndProjectSteward', N'IsRestrictedToAdminAndProjectStewardAndCanViewLandownerInfo', 'COLUMN' 
+
