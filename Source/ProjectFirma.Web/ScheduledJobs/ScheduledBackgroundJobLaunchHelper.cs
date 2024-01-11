06e5d5b6206af48eb57f53109f13df39d0d4aa89
tomkamin <tom.kamin@sitkatech.com>
2024-01-10 17:47:12 -0800

    WADNR-2095 Allow deletion of multiple projects from Program Project grid setting up bulk delete background job

--- Source/ProjectFirma.Web/ScheduledJobs/ScheduledBackgroundJobLaunchHelper.cs
+++ Source/ProjectFirma.Web/ScheduledJobs/ScheduledBackgroundJobLaunchHelper.cs
@@ -12,6 +12,11 @@ namespace ProjectFirma.Web.ScheduledJobs
             LoaDataImportBackgroundJob.Instance.RunJob(jobCancellationToken);
         }
 
+        public static void RunProjectBulkDeleteScheduledBackgroundJob(IJobCancellationToken jobCancellationToken)
+        {
+            ProjectBulkDeleteBackgroundJob.Instance.RunJob(jobCancellationToken);
+        }
+
         public static void RunProgramNotificationScheduledBackgroundJob(IJobCancellationToken jobCancellationToken)
         {
             ProgramNotificationScheduledBackgroundJob.Instance.RunJob(jobCancellationToken);
