1a19aa5e8b9bb17a4990b5ee46c49471aa6988d3
tomkamin <tom.kamin@sitkatech.com>
2024-01-18 17:36:28 -0800

    WADNR-2102 Add Priority Landscapes to FHT release script to add data to new priority landscapes

---
+++ Database/ReleaseScript/0655 -update new priority landscapes with appropriate above map text.sql
@@ -0,0 +1,3 @@
+
+
+  update dbo.PriorityLandscape set PriorityLandscapeAboveMapText = 'This map displays the simple location of forest health projects in this priority landscape along with optional additional layers that users can select to view including detailed project and treatment locations.' where PriorityLandscapeAboveMapText is null 
