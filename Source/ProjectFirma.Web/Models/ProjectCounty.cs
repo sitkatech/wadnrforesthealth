1599671601aaaa7ecb65d052fcc4a05225d230b4
tomkamin <tom.kamin@sitkatech.com>
2024-03-18 16:05:07 -0700

    WADNR-2057 Add audit log messages for projects created/updated via import updated models to grab entity from DB when null for audit log description strings

--- Source/ProjectFirma.Web/Models/ProjectCounty.cs
+++ Source/ProjectFirma.Web/Models/ProjectCounty.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using LtInfo.Common.Views;
+using ProjectFirma.Web.Common;
 
 namespace ProjectFirma.Web.Models
 {
@@ -10,9 +11,34 @@ namespace ProjectFirma.Web.Models
         {
             get
             {
-                var county = County != null ? County.DisplayName : ViewUtilities.NotFoundString;
-                var project = Project != null ? Project.DisplayName : ViewUtilities.NotFoundString;
-                return $"County: {county}, {FieldDefinition.Project.GetFieldDefinitionLabel()} Update: {project}";
+                var countyDisplayString = ViewUtilities.NotFoundString;
+                if (County is null)
+                {
+                    var county = HttpRequestStorage.DatabaseEntities.Counties.Find(CountyID);
+                    if (county != null)
+                    {
+                        countyDisplayString = county.DisplayName;
+                    }
+                }else
+                {
+                    countyDisplayString = County.DisplayName;
+                }
+
+                var projectDisplayString = ViewUtilities.NotFoundString;
+                if (Project is null)
+                {
+                    var project = HttpRequestStorage.DatabaseEntities.Projects.Find(ProjectID);
+                    if (project != null)
+                    {
+                        projectDisplayString = project.DisplayName;
+                    }
+                }
+                else
+                {
+                    projectDisplayString = Project.DisplayName;
+                }
+
+                return $"{FieldDefinition.County.GetFieldDefinitionLabel()}: {countyDisplayString}, {FieldDefinition.Project.GetFieldDefinitionLabel()}: {projectDisplayString}";
             }
         }
     }
