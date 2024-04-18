02a4da7d0cdae83b38f37741cb79f88727227f87
tomkamin <tom.kamin@sitkatech.com>
2024-04-17 17:01:58 -0700

    WADNR-2108 Tech Debt: Update jQuery to 3.6 or later moving to v 3.7.1

--- Source/ProjectFirma.Web/Scripts/_references.js
+++ Source/ProjectFirma.Web/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-1.12.4.js" />
+﻿/// <reference path="jquery-3.7.1.js" />
 /// <reference path="jquery-ui-1.11.4.js" />
 /// <reference path="jquery.fancybox.js" />
 /// <reference path="jquery.validate.js" />
