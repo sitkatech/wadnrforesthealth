c70c5291f114edc979abf6fd3071039f088a043a
tomkamin <tom.kamin@sitkatech.com>
2024-09-11 17:43:47 -0700

    WADNR-2123 Replace CKEditor with TinyMCE fixed an issue where tiny mce was removing empty html elements used for icons. I added nbsp to empty elements so tiny mce doesn't remove them

---
+++ Database/ReleaseScript/0663 - fixing an issue where tinymce was removing empty tags so filled them with non breaking spaces.sql
@@ -0,0 +1,69 @@
+
+
+  update dbo.FirmaPage set FirmaPageContent = '<p>This Project wizard is used to submit Projects. All Projects in the Planned, Implementation, or Post-Implementation stage can be submitted.&nbsp;<a class="fieldDefinitionLink" href="javascript:void(0)" onmouseover="ProjectFirma.Views.Methods.addHelpTooltipPopup(this, ''Project Stage'', ''\/FieldDefinition\/FieldDefinitionDetails\/36'', 300)" title="Click to get help on Project Stage" data-hasqtip="0">View Project Stage Definitions</a></p>
+
+<p>The various aspects of a Project that are available can be seen on the left. Several of these sections are optional, but others are required:</p>
+
+<ul>
+	<li><strong>Basics:</strong>&nbsp;<em>You must enter the Project&#39;s start and completion dates</em></li>
+	<li><strong>Location - Simple:</strong>&nbsp;<em>You must input your Project&#39;s location via an interactive map or provide a short location description</em></li>
+	<li><strong>Expenditures:</strong>&nbsp;<em>You must enter at least one Expenditure per year (if you spent nothing, enter a $0 expenditure)</em></li>
+	<li><strong>Budgets:</strong>&nbsp;<em>You must enter Grant Allocations and Project budgets</em></li>
+</ul>
+
+<p>In order to submit an update, all required information must be provided. You will see some icons next to some pages &ndash; here is what they mean:</p>
+
+<ul style="list-style-type: none; padding: 0">
+    <li class="iconRow">
+        <div class="iconDefinition"><span title="Required information has not been completely provided (not ready to submit)" class="glyphicon glyphicon-exclamation-sign">&nbsp;</span>&nbsp;</div> Required information has not been completely provided (you can''t submit this update)
+    </li>
+    <li class="iconRow">
+        <div class="iconDefinition"><span title="This section has been updated" class="glyphicon glyphicon-flag">&nbsp;</span>&nbsp;</div> This section has been updated - Use the "Show Changes" button to see changes
+    </li>
+    <li class="iconRow">
+        <div class="iconDefinition"><span title="Required information has been provided (ready to submit)" class="glyphicon glyphicon-ok">&nbsp;</span>&nbsp;</div> Required information has been provided and the page is complete
+    </li>
+</ul>
+
+<p>Submitting this update will notify the reviewer. Your updates will be approved or returned to you for corrections/clarifications. The official Project record will not be modified until the update is approved.</p>
+
+<p>Click the&nbsp;&nbsp;icon to view field-specific help.</p>
+
+<p>If you have questions or run into problems, please&nbsp;<a href="https://foresthealthtracker.dnr.wa.gov/Help/Support" onclick="return modalDialogLink(this, null, null);">Request Support</a>.</p>'
+
+where FirmaPageTypeID = 45
+
+
+  update dbo.FirmaPage set FirmaPageContent = '<p>This Project wizard is used to submit Projects. All Projects in the Planned, Implementation, or Post-Implementation stage can be submitted.&nbsp;<a class="fieldDefinitionLink" href="javascript:void(0)" onmouseover="ProjectFirma.Views.Methods.addHelpTooltipPopup(this, ''Project Stage'', ''\/FieldDefinition\/FieldDefinitionDetails\/36'', 300)" title="Click to get help on Project Stage" data-hasqtip="0">View Project Stage Definitions</a></p>
+
+<p>The various aspects of a Project that are available can be seen on the left. Several of these sections are optional, but others are required:</p>
+
+<ul>
+	<li><strong>Basics:</strong>&nbsp;<em>You must enter the Project&#39;s start and completion dates</em></li>
+	<li><strong>Location - Simple:</strong>&nbsp;<em>You must input your Project&#39;s location via an interactive map or provide a short location description</em></li>
+	<li><strong>Expenditures:</strong>&nbsp;<em>You must enter at least one Expenditure per year (if you spent nothing, enter a $0 expenditure)</em></li>
+	<li><strong>Budgets:</strong>&nbsp;<em>You must enter Grant Allocations and Project budgets</em></li>
+</ul>
+
+<p>In order to submit an update, all required information must be provided. You will see some icons next to some pages &ndash; here is what they mean:</p>
+
+<ul style="list-style-type: none; padding: 0">
+    <li class="iconRow">
+        <div class="iconDefinition"><span title="Required information has not been completely provided (not ready to submit)" class="glyphicon glyphicon-exclamation-sign">&nbsp;</span>&nbsp;</div> Required information has not been completely provided (you can''t submit this update)
+    </li>
+    <li class="iconRow">
+        <div class="iconDefinition"><span title="This section has been updated" class="glyphicon glyphicon-flag">&nbsp;</span>&nbsp;</div> This section has been updated - Use the "Show Changes" button to see changes
+    </li>
+    <li class="iconRow">
+        <div class="iconDefinition"><span title="Required information has been provided (ready to submit)" class="glyphicon glyphicon-ok">&nbsp;</span>&nbsp;</div> Required information has been provided and the page is complete
+    </li>
+</ul>
+
+<p>Submitting this update will notify the reviewer. Your updates will be approved or returned to you for corrections/clarifications. The official Project record will not be modified until the update is approved.</p>
+
+<p>Click the&nbsp;&nbsp;icon to view field-specific help.</p>
+
+<p>If you have questions or run into problems, please&nbsp;<a href="https://foresthealthtracker.dnr.wa.gov/Help/Support" onclick="return modalDialogLink(this, null, null);">Request Support</a>.</p>'
+
+where FirmaPageTypeID = 47
+
