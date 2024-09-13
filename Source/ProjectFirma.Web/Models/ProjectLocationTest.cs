192f41dc93d03ef126c7b019b233bccd4d4ce324
tomkamin <tom.kamin@sitkatech.com>
2024-09-13 15:25:57 -0700

    adding comment with SQL to fix bad project locations

--- Source/ProjectFirma.Web/Models/ProjectLocationTest.cs
+++ Source/ProjectFirma.Web/Models/ProjectLocationTest.cs
@@ -8,6 +8,9 @@ namespace ProjectFirma.Web.Models
     [TestFixture]
     public class ProjectLocationTest : DatabaseDirectAccessTestFixtureBase
     {
+        /// <summary>
+        /// If this unit test find any bad project locations, they can be fixed by running the SQL commented out in the test below.
+        /// </summary>
         [Test]
         [UseReporter(typeof(DiffReporter))]
         public void AreProjectLocationsNotBad()
@@ -29,6 +32,29 @@ namespace ProjectFirma.Web.Models
                         order by p.ProjectID";
             var result = ExecAdHocSql(sqlQueryString);
             Approvals.Verify(result.TableToHumanReadableString());
+
+            // The following SQL can be used to fix the bad project locations found by the test above.
+            //drop table if exists #tmpA
+            //    select p.ProjectID,
+            //p.ProjectName, 
+            //p.ProjectLocationPoint.STDistance(l.ProjectLocationGeometry.STCentroid()) as Distance,
+            //p.ProjectLocationPoint, l.ProjectLocationGeometry, l.ProjectLocationGeometry.STCentroid() as BetterPoint
+            //into #tmpA
+            //    from dbo.Project p
+            //join(
+            //    SELECT l.ProjectID, geometry::UnionAggregate(l.ProjectLocationGeometry) as ProjectLocationGeometry
+            //from dbo.vGeoServerProjectLocationDetailed l
+            //    group by l.ProjectID
+            //    ) l on p.ProjectID = l.ProjectID
+            //where p.ProjectLocationPoint.STIntersects(l.ProjectLocationGeometry) = 0
+            //and p.ProjectLocationPoint.STDistance(l.ProjectLocationGeometry.STCentroid())
+            //    > 0.001
+
+            //update dbo.Project
+            //set ProjectLocationPoint = BetterPoint
+            //from #tmpA x
+            //join dbo.Project p on x.ProjectID = p.ProjectID
+            //where p.CreateGisUploadAttemptID is not null
         }
         [TestFixtureSetUp]
         public void TestFixtureSetup()
