5e0598b13480d555d9db174e7e4e77e8059fa4d3
tomkamin <tom.kamin@sitkatech.com>
2024-12-27 12:25:38 -0800

    commenting this script out so release works

--- Database/ReleaseScript/0666 - new banner logo.sql
+++ Database/ReleaseScript/0666 - new banner logo.sql
@@ -1,28 +1,28 @@
 
-DECLARE @fileBinary varbinary(max)
+--DECLARE @fileBinary varbinary(max)
 
-IF @@SERVERNAME = 'DESCHUTES' or @@SERVERNAME = 'KETTLE'
-    SET @fileBinary =  (SELECT bulkcolumn
-        FROM OPENROWSET( BULK N'C:\Sitka\WADNRForestHealth\tempForReleaseScript\wa_dnr_logo2.png', SINGLE_BLOB ) AS y );
-ELSE
-    SET @fileBinary =  (SELECT bulkcolumn
-        FROM OPENROWSET( BULK N'C:\git\sitkatech\wadnrforesthealth\Source\ProjectFirma.Web\Content\img\wa_dnr_logo2.png', SINGLE_BLOB ) AS y );
+--IF @@SERVERNAME = 'DESCHUTES' or @@SERVERNAME = 'KETTLE'
+--    SET @fileBinary =  (SELECT bulkcolumn
+--        FROM OPENROWSET( BULK N'C:\Sitka\WADNRForestHealth\tempForReleaseScript\wa_dnr_logo2.png', SINGLE_BLOB ) AS y );
+--ELSE
+--    SET @fileBinary =  (SELECT bulkcolumn
+--        FROM OPENROWSET( BULK N'C:\git\sitkatech\wadnrforesthealth\Source\ProjectFirma.Web\Content\img\wa_dnr_logo2.png', SINGLE_BLOB ) AS y );
     
-insert into dbo.FileResource (FileResourceMimeTypeID, OriginalBaseFilename, OriginalFileExtension, FileResourceGUID, FileResourceData, CreatePersonID, CreateDate)
-values
-(
-    5,
-    'Washington_State_Department_of_Natural_Resources_logo2',
-    '.png',
-    NEWID(),
-    @fileBinary,
-    5233,
-    CURRENT_TIMESTAMP
-);
+--insert into dbo.FileResource (FileResourceMimeTypeID, OriginalBaseFilename, OriginalFileExtension, FileResourceGUID, FileResourceData, CreatePersonID, CreateDate)
+--values
+--(
+--    5,
+--    'Washington_State_Department_of_Natural_Resources_logo2',
+--    '.png',
+--    NEWID(),
+--    @fileBinary,
+--    5233,
+--    CURRENT_TIMESTAMP
+--);
 
-update dbo.SystemAttribute
-set BannerLogoFileResourceID = SCOPE_IDENTITY()
-where SystemAttributeID = 10
+--update dbo.SystemAttribute
+--set BannerLogoFileResourceID = SCOPE_IDENTITY()
+--where SystemAttributeID = 10
 
 /*
 
