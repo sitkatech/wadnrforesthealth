f8dc396547fa0729b561b6b882d619a49c75e94d
tomkamin <tom.kamin@sitkatech.com>
2025-06-11 18:44:29 -0700

    WADNR-2180 Simplify Add a Project Workflow - Location - Simple

--- Source/ProjectFirma.Web/Views/Shared/ProjectLocationControls/ProjectLocationSimpleController.js
+++ Source/ProjectFirma.Web/Views/Shared/ProjectLocationControls/ProjectLocationSimpleController.js
@@ -41,6 +41,20 @@
                         $scope.projectLocationMap.map.removeLayer($scope.projectLocationMap.currentSelectedGeometry);
                     }
                     break;
+                case "LatLngInput":
+                    document.getElementById($scope.AngularViewData.MapInitJson.MapDivID).style.cursor = "default";
+                    $scope.projectLocationMap.unblockMap();
+                    $scope.projectLocationMap.map.scrollWheelZoom.enable();
+                    if ($scope.projectLocationMap.currentSelectedPoint) {
+                        $scope.projectLocationMap.map.addLayer($scope.projectLocationMap.currentSelectedPoint);
+                        $scope.AngularModel.ProjectLocationPointX = L.Util.formatNum($scope.projectLocationMap.currentSelectedPoint._latlng.lng);
+                        $scope.AngularModel.ProjectLocationPointY = L.Util.formatNum($scope.projectLocationMap.currentSelectedPoint._latlng.lat);
+                    }
+
+                    if ($scope.projectLocationMap.currentSelectedGeometry) {
+                        $scope.projectLocationMap.map.removeLayer($scope.projectLocationMap.currentSelectedGeometry);
+                    }
+                    break;
                 case "None":
                     document.getElementById($scope.AngularViewData.MapInitJson.MapDivID).style.cursor = "default";
                     $scope.projectLocationMap.map.scrollWheelZoom.disable();
@@ -57,6 +71,52 @@
                 }
             };
 
+            $scope.changedLatLngInput = function () {
+                if (isNaN($scope.AngularModel.ProjectLocationPointY) || isNaN($scope.AngularModel.ProjectLocationPointX) || $scope.AngularModel.ProjectLocationPointY.toString().slice(-1) === "." || $scope.AngularModel.ProjectLocationPointX.toString().slice(-1) === ".") {
+                    return;
+                }
+                // if either the lat or lng are falsy we should remove the selected point from the map
+                if ((!$scope.AngularModel.ProjectLocationPointY || !$scope.AngularModel.ProjectLocationPointX) && $scope.projectLocationMap.currentSelectedPoint) {
+                    $scope.projectLocationMap.map.removeLayer($scope.projectLocationMap.currentSelectedPoint);
+                    return;
+                }
+
+                var latlng = new L.LatLng($scope.AngularModel.ProjectLocationPointY, $scope.AngularModel.ProjectLocationPointX);
+
+                // if we haven't returned (the lat lng numbers look good enough for the setPointOnMap function) we can set the point on the map
+                setPointOnMap(latlng);
+            }
+
+            $scope.checkInputsForDegreeMinutes = function() {
+                var regex = /(-*)*(\d*)[° ] *(\d*)[' ] *(\d*\.?\d*)["]? *([NnSsEeWw])?/; // https://regex101.com/r/rH0JLy/3
+
+                if ($scope.AngularModel.ProjectLocationPointY) {
+                    var latMatch = $scope.AngularModel.ProjectLocationPointY.toString().match(regex);
+                    if (latMatch) {
+                        var latDegrees = parseFloat(latMatch[2]);
+                        var latMinutes = parseFloat(latMatch[3]);
+                        var latSeconds = parseFloat(latMatch[4]);
+                        var latIsNegative = (latMatch[5] && latMatch[5].toLowerCase() === "s" ) || latMatch[1] === "-";
+                        var latDecimalValue = latDegrees + (latMinutes / 60) + (latSeconds / 3600);
+                        $scope.AngularModel.ProjectLocationPointY = latIsNegative ? -latDecimalValue : latDecimalValue;
+                        $scope.changedLatLngInput();
+                    }
+                }
+
+                if ($scope.AngularModel.ProjectLocationPointX) {
+                    var lonMatch = $scope.AngularModel.ProjectLocationPointX.toString().match(regex);
+                    if (lonMatch) {
+                        var lonDegrees = parseFloat(lonMatch[2]);
+                        var lonMinutes = parseFloat(lonMatch[3]);
+                        var lonSeconds = parseFloat(lonMatch[4]);
+                        var lonIsNegative = (lonMatch[5] && lonMatch[5].toLowerCase() === "w") || lonMatch[1] === "-";
+                        var lonDecimalValue = lonDegrees + (lonMinutes / 60) + (lonSeconds / 3600);
+                        $scope.AngularModel.ProjectLocationPointX = lonIsNegative ? -lonDecimalValue : lonDecimalValue;
+                        $scope.changedLatLngInput();
+                    }
+                }
+            }
+
             function onMapClick(event) {
                 switch ($scope.AngularModel.ProjectLocationSimpleType) {
                 case "PointOnMap":
@@ -65,6 +125,7 @@
                             $scope.$apply();
                         });
                     break;
+                case "LatLngInput":
                 case "None": // Do nothing
                     break;
                 }
@@ -72,59 +133,11 @@
 
             function onMapClickSetPointOnMap(event, callback) {
                 var latlng = event.latlng;
-                var latlngWrapped = latlng.wrap();
-
-                var propertiesForDisplay = {
-                    Latitude: L.Util.formatNum(latlngWrapped.lat, 4),
-                    Longitude: L.Util.formatNum(latlngWrapped.lng, 4)
-                };
-
-                if (!$scope.hasGeospatialAreaTypeLayers) {
-                    setPointOnMap(latlng);
-                    $scope.propertiesForPointOnMap = propertiesForDisplay;
-                    if (callback) {
-                        callback.call();
-                    }
-                }
-                else {
-                    var parameters = L.Util.extend($scope.projectLocationMap.wfsParams,
-                        {
-                            typeName: $scope.GeospatialAreaMapServiceLayerNamesCommaSeparated,
-                            cql_filter: "intersects(Ogr_Geometry, POINT(" +
-                                latlngWrapped.lat +
-                                " " +
-                                latlngWrapped.lng +
-                                "))"
-                        });
-                    SitkaAjax.ajax({
-                        url: $scope.AngularViewData.MapServiceUrl + L.Util.getParamString(parameters),
-                            dataType: "json",
-                            jsonpCallback: "getJson"
-                        },
-                        function(response) {
-                            setPointOnMap(latlng);
-                            if (response.features.length > 0) {
-                                var mergedProperties = _.merge.apply(_, _.map(response.features, "properties"));
-                                propertiesForDisplay[$scope.AngularViewData.GeospatialAreaFieldDefinitionLabel] =
-                                    mergedProperties.GeospatialAreaName;
-                            }
-
-                            $scope.propertiesForPointOnMap = propertiesForDisplay;
-
-                            if (callback) {
-                                callback.call();
-                            }
-                        },
-                        function() {
-                            console.error(
-                                "There was an error selecting the " +
-                                $scope.AngularViewData.ProjectLocationFieldDefinitionLabel +
-                                " area from list.");
-                        });
-                }
+                
+                setPointOnMap(latlng, callback);
             }
 
-            function setPointOnMap(latlng) {
+            function setPointOnMap(latlng, callback) {
                 $scope.AngularModel.ProjectLocationPointX = L.Util.formatNum(latlng.lng);
                 $scope.AngularModel.ProjectLocationPointY = L.Util.formatNum(latlng.lat);
 
@@ -144,11 +157,81 @@
 
                 $scope.projectLocationMap.map.addLayer($scope.projectLocationMap.currentSelectedPoint);
                 $scope.projectLocationMap.map.panTo(latlng);
+
+
+                var latlngWrapped = latlng.wrap();
+                var propertiesForDisplay = {
+                    Latitude: L.Util.formatNum(latlngWrapped.lat, 4),
+                    Longitude: L.Util.formatNum(latlngWrapped.lng, 4)
+                };
+
+                if (!$scope.hasGeospatialAreaTypeLayers) {
+                    $scope.propertiesForPointOnMap = propertiesForDisplay;
+                    if (callback) {
+                        callback.call();
+                    }
+                }
+                else {
+                    var parameters = L.Util.extend($scope.projectLocationMap.wfsParams,
+                        {
+                            typeName: $scope.GeospatialAreaMapServiceLayerNamesCommaSeparated,
+                            cql_filter: "intersects(Ogr_Geometry, POINT(" +
+                                latlngWrapped.lat +
+                                " " +
+                                latlngWrapped.lng +
+                                "))"
+                        });
+                    SitkaAjax.ajax({
+                        url: $scope.AngularViewData.MapServiceUrl + L.Util.getParamString(parameters),
+                        dataType: "json",
+                        jsonpCallback: "getJson"
+                    },
+                        function (response) {
+                            //debugger;
+                            if (response.features.length > 0) {
+                                var mergedProperties = _.merge.apply(_, _.map(response.features, "properties"));
+                                if (mergedProperties.DNRUplandRegionName) {
+                                    propertiesForDisplay["DNR Upland Region"] = mergedProperties.DNRUplandRegionName;
+                                } else {
+                                    propertiesForDisplay["DNR Upland Region"] = "None";
+                                }
+                                if (mergedProperties.PriorityLandscapeName) {
+                                    propertiesForDisplay["Priority Landscape"] = mergedProperties.PriorityLandscapeName;
+                                } else {
+                                    propertiesForDisplay["Priority Landscape"] = "None";
+                                }
+                                if (mergedProperties.CountyName) {
+                                    propertiesForDisplay["County"] = mergedProperties.CountyName;
+                                } else {
+                                    propertiesForDisplay["County"] = "None";
+                                }
+
+                            } else {
+                                propertiesForDisplay["DNR Upland Region"] = "None";
+                                propertiesForDisplay["Priority Landscape"] = "None";
+                                propertiesForDisplay["County"] = "None";
+                            }
+
+                            $scope.propertiesForPointOnMap = propertiesForDisplay;
+
+                            if (callback) {
+                                callback.call();
+                            }
+                        },
+                        function () {
+                            console.error(
+                                "There was an error selecting the " +
+                                $scope.AngularViewData.ProjectLocationFieldDefinitionLabel +
+                                " area from list.");
+                        });
+                }
+
             }
 
             $scope.getProjectLocationProperties = function() {
                 switch ($scope.AngularModel.ProjectLocationSimpleType) {
                 case "PointOnMap":
+                    case "LatLngInput":
                     if ($scope.propertiesForPointOnMap === undefined) {
                         return $scope.propertiesForPointOnMap;
                     }
@@ -163,6 +246,10 @@
                 }
                 return null;
             };
+
+            $scope.displayLatLngInputs = function() {
+                return ($scope.AngularModel.ProjectLocationSimpleType === "LatLngInput");
+            }
 
             ProjectFirmaMaps.Map.prototype.handleWmsPopupClickEventWithCurrentLayer  = function() {
                 // Override parent to do nothing
@@ -189,38 +276,12 @@
 
                 if ($scope.AngularModel.ProjectLocationPointX && $scope.AngularModel.ProjectLocationPointY) {
                     var latlng = new L.LatLng($scope.AngularModel.ProjectLocationPointY, $scope.AngularModel.ProjectLocationPointX);
-                    var latlngWrapped = latlng.wrap();
-                    $scope.projectLocationMap.currentSelectedPoint = L.marker(latlng, { icon: L.MakiMarkers.icon({ icon: "marker", color: $scope.selectedStyle.color, size: "m" }) });
-
-                    $scope.propertiesForPointOnMap = {
-                        Latitude: L.Util.formatNum(latlngWrapped.lat, 4),
-                        Longitude: L.Util.formatNum(latlngWrapped.lng, 4)
-                    };
+                    //$scope.projectLocationMap.currentSelectedPoint = L.marker(latlng, { icon: L.MakiMarkers.icon({ icon: "marker", color: $scope.selectedStyle.color, size: "m" }) });
 
                     // Get the initial Location Information from the WMS service
-                    if ($scope.hasGeospatialAreaTypeLayers) {
-                        SitkaAjax.ajax({
-                                url: $scope.AngularViewData.MapServiceUrl +
-                                    L.Util.getParamString(L.Util.extend($scope.projectLocationMap.wfsParams,
-                                        {
-                                            typeName: $scope.GeospatialAreaMapServiceLayerNamesCommaSeparated,
-                                            cql_filter: "intersects(Ogr_Geometry, POINT(" + latlngWrapped.lat + " " + latlngWrapped.lng + "))"
-                                        })),
-                                dataType: "json",
-                                jsonpCallback: "getJson"
-                            },
-                            function(response) {
-                                if (response.features.length === 0)
-                                    return;
-
-                                var mergedProperties = _.merge.apply(_, _.map(response.features, "properties"));
-                                $scope.propertiesForPointOnMap[$scope.AngularViewData.GeospatialAreaFieldDefinitionLabel] = mergedProperties.GeospatialAreaName;
-                                $scope.$apply();
-                            },
-                            function() {
-                                console.error("There was an error getting the initial " + $scope.AngularViewData.GeospatialAreaFieldDefinitionLabel + " name to display.");
-                            });
-                    }
+                    setPointOnMap(latlng, function() {
+                        $scope.$apply();
+                    });
                 }
             };
 
